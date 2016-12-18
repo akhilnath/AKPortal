2b94105cae3b21f2e701862114bad68a21e60ece
akhilnath <muddanaakhilnath@gmail.com>
2016-12-18 16:31:15 -0500

    changed filter configs

--- vidly2/vidly2/App_Start/FilterConfig.cs
+++ vidly2/vidly2/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace vidly2
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
 		{
 			filters.Add(new HandleErrorAttribute());
+//should handle authentication
 		}
 	}
 }
